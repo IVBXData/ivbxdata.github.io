1b19cc8873e0390d2c1d0b033e9f47f4ebc091bd
IVBX Data <89563977+IVBXData@users.noreply.github.com>
2024-12-31 11:50:50 +0800

    Add files via upload

---
+++ Dev/service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'ivbx-server-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'index.css',
+  'icon.ico',
+  'icon.png',
+  'download-icon.png',
+  'team-icon.png',
+  'note-icon.png',
+  'calc-icon.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
